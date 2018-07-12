71ff8ace8c6734b57ae84e91ff38e04d9ba82619
github username <github email address>
2018-07-12 15:36:01 -0400

    added search bars for recipelist

--- src/containers/RecipeList.js
+++ src/containers/RecipeList.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import Recipe from '../components/Recipe';
 import RecipeDetails from '../components/RecipeDetails';
-import { Card } from 'semantic-ui-react';
+import { Card, Input } from 'semantic-ui-react';
+// import './stylesheets/index.css';
+// import 'semantic-ui-css/semantic.min.css'
 
 class RecipeList extends React.Component {
   state = {
     recipes: [],
+    filteredRecipes: [],
     selectedRecipe: null,
-    recipeList: "true"
+    recipeList: "true",
+    searchTerm: "",
+    searchTermIng: ""
   }
 
   componentDidMount() {
@@ -31,6 +36,7 @@ class RecipeList extends React.Component {
     .then(json => {
       this.setState({
         recipes: json,
+        filteredRecipes: json
       });
     })
     ///////////////// YO WHAT IS THIS /////////////////////////////////////////////
@@ -63,11 +69,62 @@ class RecipeList extends React.Component {
     })
   }
 
+  handleChange = (event) => {
+
+    const recipes = this.state.filteredRecipes.filter( recipe => {
+      console.log("recipe", recipe)
+      return recipe.label.toLowerCase().includes(event.target.value.toLowerCase())
+    })
+
+    this.setState({
+      searchTerm: event.target.value,
+      recipes
+    })
+}
+
+    handleChangeIng = (event) => {
+
+      const recipes = this.state.filteredRecipes.filter( recipe => {
+        return recipe.ingredients.toLowerCase().includes(event.target.value.toLowerCase())
+      })
+      this.setState({
+        searchTermIng: event.target.value,
+        recipes
+      })
+    }
+
+
+
+
+
+
   render() {
     console.log('all my recipes inside my list', this.state.recipes)
     let allRecipes = this.state.recipes.map(recipe => <Recipe recipe={recipe} selectRecipe={() => this.selectRecipe(recipe)} />)
     return (
       <React.Fragment>
+
+
+        <div>
+          <Input
+             style={{padding:'10px'}}
+             size='massive'
+             onChange={this.handleChange}
+             placeholder='Search By Name...'
+             value={this.state.searchTerm}
+             type="text"
+          />
+          <Input
+            style={{padding:'10px'}}
+            size='massive'
+            onChange={this.handleChangeIng}
+            placeholder='Search By Ingredients...'
+            value={this.state.searchTermIng}
+            type="text"
+          />
+
+        </div>
+
         {this.state.selectedRecipe !== null ? <RecipeDetails recipeList={this.state.recipeList} clearRecipe={this.clearRecipe} recipe={this.state.selectedRecipe} /> :
         <div>
           <h2>My Recipes</h2>
